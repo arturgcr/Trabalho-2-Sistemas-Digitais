6985cdbb8e21d43e1fbdcdb9bba03ff89c9e0a24
Arturgcr <arturgcr78@gmail.com>
2025-06-23 15:53:03 -0300

    ADD; Adicionada estrutura inicial de arquivos

---
+++ calculadora_fpga/src/ascii_converter.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:46:41 06/23/2025 
+-- Design Name: 
+-- Module Name:    ascii_converter - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ascii_converter is
+end ascii_converter;
+
+architecture Behavioral of ascii_converter is
+
+begin
+
+
+end Behavioral;
+
---
+++ calculadora_fpga/src/teclado_ps2.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:45:00 06/23/2025 
+-- Design Name: 
+-- Module Name:    teclado_ps2 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity teclado_ps2 is
+end teclado_ps2;
+
+architecture Behavioral of teclado_ps2 is
+
+begin
+
+
+end Behavioral;
+
